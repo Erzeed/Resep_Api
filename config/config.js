f60c1fe81795f3b47ddc9df90472f5ad525fe678
Erzed <feizal.reza@students.amikom.ac.id>
2021-06-21 22:08:01 +0700

    firs commit

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI : `mongodb://Erzeed:3cf3J8ntj2vKSh0e@leraning-shard-00-00.6by9x.mongodb.net:27017,leraning-shard-00-01.6by9x.mongodb.net:27017,leraning-shard-00-02.6by9x.mongodb.net:27017/ResepKuy?ssl=true&replicaSet=atlas-vi1whl-shard-0&authSource=admin&retryWrites=true&w=majority`
+};
+
+//3cf3J8ntj2vKSh0e
