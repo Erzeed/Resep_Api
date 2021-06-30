c8b0af9c3386dc1d81975bbc1dba5c7696e94cf0
Erzed <feizal.reza@students.amikom.ac.id>
2021-06-30 15:15:05 +0700

    update

--- model/dataResepSchema.js
+++ model/dataResepSchema.js
@@ -26,6 +26,11 @@ var resep = new mongoose.Schema({
         type:String,
         required:true,
     },
+    description:{
+        type:String,
+        required:true,
+    },
+    
     difficulty:{
         type:String,
         required:true,
